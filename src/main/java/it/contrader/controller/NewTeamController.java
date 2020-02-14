58cda75af845919aa80bc126e42ff05dc497ab02
Salvo Furnari <xsaf94x@gmail.com>
2020-02-14 13:38:16 +0100

    Auto stash before merge of "secretcode-console" and "origin/secretcode-console"

--- src/main/java/it/contrader/controller/NewTeamController.java
+++ src/main/java/it/contrader/controller/NewTeamController.java
@@ -1,64 +1,64 @@
 
-package it.contrader.controller;//view-service
-
-import it.contrader.main.MainDispatcher;
-import it.contrader.service.NewTeamService;
-import it.contrader.dto.NewTeamDTO;
-
-public class NewTeamController implements Controller {
-	private static String sub_package = "newTeam."; 
-	private NewTeamService newTeamService;
-	
-	public NewTeamController() {
-		this.newTeamService = new NewTeamService();
-	}
-	private int idteam;
-	private int iduser;
-	private String usertype;
-
-	public void doControl (Request request) {
-		
-		String var=request.get("var").toString();
-		switch (var.toUpperCase()){
-		
+package it.contrader.controller;//view-service
+
+import it.contrader.main.MainDispatcher;
+import it.contrader.service.NewTeamService;
+import it.contrader.dto.NewTeamDTO;
+
+public class NewTeamController implements Controller {
+	private static String sub_package = "newTeam."; 
+	private NewTeamService newTeamService;
+	
+	public NewTeamController() {
+		this.newTeamService = new NewTeamService();
+	}
+	private int idteam;
+	private int iduser;
+	private String usertype;
+
+	public void doControl (Request request) {
+		
+		String var=request.get("var").toString();
+		switch (var.toUpperCase()){
+		
 	    case "READ":
-	    	
-			iduser = Integer.parseInt(request.get("iduser").toString());
-			NewTeamDTO newTeamDTO = newTeamService.read(iduser);
-			request.put("utentiteam", newTeamDTO);
+	    	
+			iduser = Integer.parseInt(request.get("iduser").toString());
+			NewTeamDTO newTeamDTO = newTeamService.read(iduser);
+			request.put("utentiteam", newTeamDTO);
 			MainDispatcher.getInstance().callView("ReadNewTeam", request);
-			break;
-	    case "DELETE":
-	    	iduser = Integer.parseInt(request.get("iduser").toString());
-	    	idteam = Integer.parseInt(request.get("idteam").toString());
-	    	newTeamDTO=new NewTeamDTO(idteam,iduser, "");
+			break;
+	    case "DELETE":
+	    	iduser = Integer.parseInt(request.get("iduser").toString());
+	    	idteam = Integer.parseInt(request.get("idteam").toString());
+	    	newTeamDTO=new NewTeamDTO(idteam,iduser, "");
 	    	 boolean res= newTeamService.delete(newTeamDTO);
-	    	request.put("res", res);
-	    	MainDispatcher.getInstance().callView("DeleteNewTeam", request);
-			break;
-	    case "UPDATE":
-	    	this.iduser = Integer.parseInt(request.get("iduser").toString());
-       	    this.usertype = request.get("usertype").toString();
-       	    this.idteam = Integer.parseInt(request.get("idteam").toString());
-       	    newTeamDTO=new NewTeamDTO(idteam,iduser,usertype);
+	    	request.put("res", res);
+	    	MainDispatcher.getInstance().callView("DeleteNewTeam", request);
+			break;
+	    case "UPDATE":
+	    	this.iduser = Integer.parseInt(request.get("iduser").toString());
+       	    this.usertype = request.get("usertype").toString();
+       	    this.idteam = Integer.parseInt(request.get("idteam").toString());
+       	    newTeamDTO=new NewTeamDTO(idteam,iduser,usertype);
        	    this.newTeamService.update(newTeamDTO);
        	    boolean mod= newTeamService.update(newTeamDTO);
 	    	request.put("mod", mod);
-	    	MainDispatcher.getInstance().callView("MTeam",request);
-			break;
-			
-        case "INSERT":
-        	 this.iduser = Integer.parseInt(request.get("iduser").toString());
-        	 this.usertype = request.get("usertype").toString();
-        	 this.idteam = Integer.parseInt(request.get("idteam").toString());
-        	 newTeamDTO=new NewTeamDTO(idteam,iduser,usertype);
-        	 this.newTeamService.insert(newTeamDTO);
+	    	MainDispatcher.getInstance().callView("MTeam",request);
+			break;
+			
+        case "INSERT":
+        	 this.iduser = Integer.parseInt(request.get("iduser").toString());
+        	 this.usertype = request.get("usertype").toString();
+        	 this.idteam = Integer.parseInt(request.get("idteam").toString());
+        	 newTeamDTO=new NewTeamDTO(idteam,iduser,usertype);
+        	 this.newTeamService.insert(newTeamDTO);
         	 boolean ins= newTeamService.update(newTeamDTO);
  	    	 request.put("ins", ins);
         	 MainDispatcher.getInstance().callView("InsNewTeam",request);
-        	 break;
-	    }
-
-	}
-
+        	 break;
+	    }
+
+	}
+
 }
--- src/main/java/it/contrader/service/NewTeamService.java
+++ src/main/java/it/contrader/service/NewTeamService.java
@@ -1,44 +1,44 @@
 
-package it.contrader.service;
-
-
-
-import it.contrader.converter.NewTeamConverter;
-import it.contrader.dao.NewTeamDAO;
-import it.contrader.dto.NewTeamDTO;
-
-public class NewTeamService {
-	
-	private NewTeamDAO newTeamDAO;
-	private NewTeamConverter newTeamConverter;
-	
-	
-	public NewTeamService(){
-		this.newTeamDAO = new NewTeamDAO();
-		this.newTeamConverter = new NewTeamConverter();
-	}
-	
-	public NewTeamDTO read(int iduser) {
-		return newTeamConverter.toDTO(newTeamDAO.read(iduser));
-	}
-
-
-	public boolean insert(NewTeamDTO dto) {
-		return newTeamDAO.insert(newTeamConverter.toEntity(dto));
-	}
-
-
-	public boolean update(NewTeamDTO dto) {
-		return newTeamDAO.update(newTeamConverter.toEntity(dto));
-	}
-
-
-	public boolean delete(NewTeamDTO dto) {
-		return newTeamDAO.delete(newTeamConverter.toEntity(dto));
-	}
-	
-
-}
-		
+package it.contrader.service;
+
+
+
+import it.contrader.converter.NewTeamConverter;
+import it.contrader.dao.NewTeamDAO;
+import it.contrader.dto.NewTeamDTO;
+
+public class NewTeamService {
+	
+	private NewTeamDAO newTeamDAO;
+	private NewTeamConverter newTeamConverter;
+	
+	
+	public NewTeamService(){
+		this.newTeamDAO = new NewTeamDAO();
+		this.newTeamConverter = new NewTeamConverter();
+	}
+	
+	public NewTeamDTO read(int iduser) {
+		return newTeamConverter.toDTO(newTeamDAO.read(iduser));
+	}
+
+
+	public boolean insert(NewTeamDTO dto) {
+		return newTeamDAO.insert(newTeamConverter.toEntity(dto));
+	}
+
+
+	public boolean update(NewTeamDTO dto) {
+		return newTeamDAO.update(newTeamConverter.toEntity(dto));
+	}
+
+
+	public boolean delete(NewTeamDTO dto) {
+		return newTeamDAO.delete(newTeamConverter.toEntity(dto));
+	}
+	
+
+}
+		
 		
 
